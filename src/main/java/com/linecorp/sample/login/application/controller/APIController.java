3e232075373464de62bce97276a52fe29f9715d1
Tuong Le Hoang Viet <tuong.le@pyramid-consulting.com>
2018-04-04 10:05:32 +0700

    update api

--- src/main/java/com/linecorp/sample/login/application/controller/APIController.java
+++ src/main/java/com/linecorp/sample/login/application/controller/APIController.java
@@ -17,8 +17,6 @@ package com.linecorp.sample.login.application.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,22 +26,19 @@ public class APIController {
     private static final String JSON_APPLICATION = "application/json";
     private static final Logger LOGGER = LoggerFactory.getLogger(APIController.class);
 
-    @RequestMapping(value ="media/callback/notify", consumes = JSON_APPLICATION, produces = JSON_APPLICATION)
-    public ResponseEntity notifyCallback(@RequestBody RequestEntity notifyRequest) {
+    @RequestMapping(value = "media/callback/notify", consumes = JSON_APPLICATION, produces = JSON_APPLICATION)
+    public void notifyCallback(@RequestBody AddMediaResponse notifyRequest) {
         LOGGER.info("notifyCallback: {}", notifyRequest);
-        return null;
     }
 
-    @RequestMapping(value ="media/callback/error", consumes = JSON_APPLICATION, produces = JSON_APPLICATION)
-    public ResponseEntity notifyCallbackError(@RequestBody RequestEntity notifyRequest) {
+    @RequestMapping(value = "media/callback/error", consumes = JSON_APPLICATION, produces = JSON_APPLICATION)
+    public void notifyCallbackError(@RequestBody AddMediaResponse notifyRequest) {
         LOGGER.info("notifyCallbackError: {}", notifyRequest);
-        return null;
     }
 
-    @RequestMapping(value ="media/callback/upload", consumes = JSON_APPLICATION, produces = JSON_APPLICATION)
-    public ResponseEntity notifyCallbackUpload(@RequestBody RequestEntity notifyRequest) {
+    @RequestMapping(value = "media/callback/upload", consumes = JSON_APPLICATION, produces = JSON_APPLICATION)
+    public void notifyCallbackUpload(@RequestBody AddMediaResponse notifyRequest) {
         LOGGER.info("notifyCallbackUpload: {}", notifyRequest);
-        return null;
     }
 
 }
---
+++ src/main/java/com/linecorp/sample/login/application/controller/AddMediaResponse.java
@@ -0,0 +1,74 @@
+package com.linecorp.sample.login.application.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
+public class AddMediaResponse {
+    private String mediaid;
+    private String source;
+    private String status;
+    private String description;
+    private String taskId;
+    
+    @JsonProperty("output_params")
+    private String outputParams;
+    
+    private Map<String, Object> format;
+
+    public String getMediaid() {
+        return mediaid;
+    }
+
+    public void setMediaid(String mediaid) {
+        this.mediaid = mediaid;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getOutputParams() {
+        return outputParams;
+    }
+
+    public void setOutputParams(String outputParams) {
+        this.outputParams = outputParams;
+    }
+
+    public Map<String, Object> getFormat() {
+        return format;
+    }
+
+    public void setFormat(Map<String, Object> format) {
+        this.format = format;
+    }
+}
